4c9ac5a6cdd7e40f805df0859e2c1619b7317c71
GamzeAkbal1 <gakbal112@gmail.com>
2017-10-19 17:32:05 +0300

    Init :beers:

---
+++ LinkedListInOrder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListInOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Adding In Order");
+        }
+    }
+}
